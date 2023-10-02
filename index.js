f5926cbd71a31b6226004fdd7899307b5a683c3d
G303K <micder18@gmail.com>
2023-10-02 17:44:38 -0400

    Re-installed inquirer & created js function

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title for your README:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description:',
+  },
+  // Add more questions for other sections of the README as needed
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function to write the README file
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to initialize the app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // Generate the README content based on user answers
+      const readmeContent = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Installation
+<!-- Add installation instructions here -->
+
+## Usage
+<!-- Add usage instructions here -->
+
+## License
+<!-- Add license information here -->
+`;
+
+      // Specify the file path where you want to save the README
+      const readmeFilePath = 'README.md';
+
+      // Write the README content to the file
+      writeToFile(readmeFilePath, readmeContent);
+
+      console.log(`README.md has been generated.`);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 
 // Function call to initialize app
 init();
