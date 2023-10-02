53a0dfaa3e8ec0f07879d9a2090e944b3145bd9b
G303K <micder18@gmail.com>
2023-10-02 19:11:59 -0400

    modified code to generate the README as a .json

--- index.mjs
+++ index.mjs
@@ -13,48 +13,48 @@ const questions = [
     name: 'description',
     message: 'Enter a description:',
   },
-  // Add more questions for other sections of the README as needed
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage instructions:',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'Enter license information:',
+  },
 ];
-
-// Create a function to write the README file
-function writeToFile(fileName, data) {
-  fs.writeFileSync(fileName, data);
-}
 
 // Create a function to initialize the app
 function init() {
-  inquirer
+  return inquirer
     .prompt(questions)
     .then((answers) => {
-      // Generate the README content based on user answers
-      const readmeContent = `
-# ${answers.title}
-
-## Description
-${answers.description}
-
-## Installation
-<!-- Add installation instructions here -->
-
-## Usage
-<!-- Add usage instructions here -->
-
-## License
-<!-- Add license information here -->
-`;
-
-      // Specify the file path where you want to save the README
-      const readmeFilePath = 'README.md';
-
-      // Write the README content to the file
-      writeToFile(readmeFilePath, readmeContent);
-
-      console.log(`README.md has been generated.`);
+      return answers;
     })
     .catch((error) => {
       console.error('Error:', error);
     });
 }
 
+// Function to save data to a JSON file
+function saveToJsonFile(fileName, data) {
+  fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
+}
+
 // Function call to initialize app
-init();
+init().then((answers) => {
+  // Specify the file path where you want to save the JSON file
+  const jsonFilePath = 'README.json';
+
+  // Save the user input data to the JSON file
+  saveToJsonFile(jsonFilePath, answers);
+
+  console.log(`README.json has been generated.`);
+});
+
